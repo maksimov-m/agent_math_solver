1573e2c865ab74998fd9b57c9d4e9be5a91a2925
maksimov maksim <villbe000@gmail.com>
2025-07-08 08:08:19 +0500

    README

--- chatbot.py
+++ chatbot.py
@@ -1,23 +1,19 @@
 #########################LangChain Code##########################
-from langchain_core.prompts import ChatPromptTemplate
 from MathAgent.agent import MathAgent
-from langchain_ollama.llms import OllamaLLM
-import time
-
-model = OllamaLLM(model="llama3.1")
 
 mathagnet = MathAgent()
 
 #########################Gradio Code##########################
 import gradio as gr
 
-def echo(message, history):
+# TODO:  добавить историю в диалог
+def solve_problem(message, history):
     state = mathagnet.graph.invoke({"messages": message})
 
     answer = state['messages'][-1].content
 
     return answer
 
-demo = gr.ChatInterface(fn=echo, type="messages", examples=["hello", "hola", "merhaba"], title="Echo Bot")
+demo = gr.ChatInterface(fn=solve_problem, type="messages", title="MathAgent")
 demo.launch()
 
